7b4a99f283e689cf9bfa18f0457240c618d718d2
Tanya Malik <tanya.21ug1040@iiitranchi.ac.in>
2023-06-01 00:16:43 +0530

    download functionality added

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,10 +1,29 @@
-export const uploadImage = (request, response) => {
+import File from "../models/file.js";
+
+export const uploadImage = async (request, response) => {
   const fileObj = {
     path: request.file.path,
     name: request.file.originalname,
   };
   try {
+    const file = await File.create(fileObj);
+    response.status(200).json({ path: `https://localhost:8000/${file._id}` });
   } catch (error) {
     console.error(error.message);
+    response.status(500).json({ error: error.message });
+  }
+};
+
+export const downloadImage = async (request, response) => {
+  try {
+    const file = File.findById(request.params.fileId);
+
+    file.downloadContent++;
+    await file.save();
+
+    response.download(file.path, file.name);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(200).json({ error: error.message });
   }
 };
