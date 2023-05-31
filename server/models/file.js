63cc2790d35530b83b499720b5664f83ce3b24d4
Tanya Malik <tanya.21ug1040@iiitranchi.ac.in>
2023-06-01 00:55:29 +0530

    final commit

--- server/models/file.js
+++ server/models/file.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const fileSchema = new mongoose.Schema({
+const FileSchema = new mongoose.Schema({
   path: {
     type: String,
     required: true,
@@ -9,13 +9,13 @@ const fileSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  downloadContent: {
+  downloadCount: {
     type: Number,
     required: true,
     default: 0,
   },
 });
 
-const File = mongoose.model("file", fileSchema);
+const File = mongoose.model("file", FileSchema);
 
 export default File;
