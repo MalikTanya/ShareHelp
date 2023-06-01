b0e630ffea9b4531727e5d5cf47dac33157c21e1
Tanya Malik <tanya.21ug1040@iiitranchi.ac.in>
2023-06-01 13:30:26 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -8,10 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const options = {
+  origin: ["http://localhost:3000", "https://share-help.vercel.app"],
+  useSuccessStatus: 200,
+};
+app.use(cors(options));
 app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use("/", router);
 
 const PORT = process.env.PORT || 8000;
