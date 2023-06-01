45620ada49a32110c5eece26b6b1cd561ee81f97
Tanya Malik <tanya.21ug1040@iiitranchi.ac.in>
2023-06-01 13:24:30 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use({ limit: "25mb" });
+app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", router);
